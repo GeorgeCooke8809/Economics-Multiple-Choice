17a14b82ad334bcc2de7591eb5018dcf128b9f64
George_Cooke <georgecooke8809@gmail.com>
2026-02-04 17:23:37 +0000

    Basic HTML and CSS for question page

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from flask import Flask, redirect
 import flask
 import backend
 
+global data
 data = backend.Data()
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET"])
+def index():
+    return flask.render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
